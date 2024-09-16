55444990966ade514f07df129168cd152459d323
itseduardolima <eduardolima2417@gmail.com>
2024-09-16 13:04:15 -0400

    update

--- mock/experiencesData.ts
+++ mock/experiencesData.ts
@@ -10,7 +10,7 @@ export const experienceData: Experience[] = [
   {
     title: "Desenvolvedor Frontend",
     company: "MW Tecnologia e Programação",
-    period: "Abr 2024 - Ago 2024",
+    period: "Jan 2024 - Ago 2024",
     description:
       "Contratado como Desenvolvedor Júnior PJ, mas as vezes desempenhando atividades de nível pleno, fui responsável por implementar e manter funcionalidades em sistemas CRMs, realizar integrações com APIs e desenvolver gráficos interativos utilizando Chart.js. Trabalhei sozinho em todo o front-end, utilizando React.js e Next.js, e focando em escrever código limpo e escalável. Desenvolvi componentes reutilizáveis para otimizar a manutenção e a eficiência do sistema, aplicando as melhores práticas de desenvolvimento.",
     technologies: [
