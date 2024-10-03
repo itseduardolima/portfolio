3d58f629fd66651d23233305a30cef0ffdb9abd8
itseduardolima <eduardolima2417@gmail.com>
2024-10-03 12:54:39 -0400

    update

--- mock/projectsData.ts
+++ mock/projectsData.ts
@@ -1,8 +1,8 @@
 import { StaticImageData } from "next/image";
 import RestaurantImage from "../public/assets/images/restaurant.png";
-import EdmindImage from "../public/assets/images/edmind.jpeg";
 import KaduFitnessImage from "../public/assets/images/kadu-fitness.jpeg";
-import WorkflowImage from "../public/assets/images/workflow-pro.png"
+import WorkflowImage from "../public/assets/images/workflow-pro.png";
+import EdSolutionsImage from "../public/assets/images/edsolutions.png";
 
 export interface Project {
   title: string;
@@ -14,6 +14,15 @@ export interface Project {
 }
 
 export const projectData: Project[] = [
+  {
+    title: "EdSoluctions",
+    description:
+      "Projeto pessoal que oferece soluções de desenvolvimento web e serviços de SEO para impulsionar a presença online do cliente.",
+    technologies: ["Next.js", "TypeScript", "Tailwind CSS", "Html"],
+    deployLink: "https://edsolutions.vercel.app/",
+    githubLink: "https://github.com/itseduardolima/edsolutions",
+    image: EdSolutionsImage,
+  },
   {
     title: "WorkflowPro",
     description:
@@ -31,15 +40,6 @@ export const projectData: Project[] = [
     deployLink: "https://restaurante-sabor-e-arte.vercel.app/",
     githubLink: "https://github.com/itseduardolima/landing-page-restaurant",
     image: RestaurantImage,
-  },
-  {
-    title: "Edmind Soluctions",
-    description:
-      "Projeto pessoal que oferece soluções de desenvolvimento web, design gráfico e serviços de SEO para impulsionar a presença online do cliente.",
-    technologies: ["React", "Vite", "TypeScript", "Styled Components", "Html"],
-    deployLink: "https://edmind.netlify.app/",
-    githubLink: "https://github.com/itseduardolima/edmind-frontend",
-    image: EdmindImage,
   },
   {
     title: "Kadu Fitness",
